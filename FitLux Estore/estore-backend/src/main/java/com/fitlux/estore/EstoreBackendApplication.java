3c3c72a8b761898036971dc7fcc9b1f5990be7cb
Prashant <prashantnaagar60@gmail.com>
2026-01-15 15:57:11 +0530

    Initial commit: FitLux backend setup and document created

---
+++ FitLux Estore/estore-backend/src/main/java/com/fitlux/estore/EstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitlux.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoreBackendApplication.class, args);
+	}
+
+}
